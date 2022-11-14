a1584ead8314b1b8fcc6928a5a5c8e6798681a8e
Kate Goldenring <kate.goldenring@fermyon.com>
2022-11-14 14:06:31 -0800

    Fix code compilation errors and add redis address via env var
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- click/src/lib.rs
+++ click/src/lib.rs
@@ -4,19 +4,22 @@ use spin_sdk::{
     http_component,
 };
 
+const REDIS_ADDRESS_ENV: &str = "REDIS_ADDRESS";
+
 /// A simple Spin HTTP component.
 #[http_component]
-fn click(req: Request) -> Result<Response> 
-    let address = // Insert Redis address here
+fn click(req: Request) -> Result<Response> {
+    let address = std::env::var(REDIS_ADDRESS_ENV)?;
 
     if let &http::Method::POST = req.method() {
         spin_sdk::redis::incr(&address, &"clicks").map_err(|_| anyhow!("Error querying Redis"))?;
     }
 
-    let clicks = spin_sdk::redis::get(&address, &"clicks").map_err(|_| anyhow!("Error querying Redis"))?;
+    let clicks =
+        spin_sdk::redis::get(&address, &"clicks").map_err(|_| anyhow!("Error querying Redis"))?;
 
     println!("{:?}", req.headers());
     Ok(http::Response::builder()
         .status(200)
         .body(Some(clicks.into()))?)
-
+}
